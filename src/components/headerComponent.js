156a1d2990be5f12befd9c29249405cdf6752f0e
Steven Boulom <sboulom@gmail.com>
2021-06-04 11:05:27 -0700

    added jumbo, nav and footer with npm packages

---
+++ src/components/headerComponent.js
@@ -0,0 +1,29 @@
+import React, { Component }  from 'react';
+import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+
+class Header extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Jumbotron fluid>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col">
+                                <h1>Boulom Kitchen</h1>
+                                <h2>Sweets to Enlighten Your Day</h2>
+                            </div>
+                        </div>
+                    </div>
+                </Jumbotron>
+
+                <Navbar dark sticky="top">
+                    <div className="container">
+                        <NavbarBrand href="/">Boulom Kitchen</NavbarBrand>
+                    </div>
+                </Navbar>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Header;
---
+++ src/components/mainComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from './headerComponent';
+import Footer from './footerComponent';
+
+class Main extends Component {
+
+    render() {
+        return (
+            <>
+                <Header />
+              
+                <Footer />
+            </>
+        );
+    }
+}
+
+export default Main;
